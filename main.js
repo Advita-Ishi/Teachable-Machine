eaacd38d935015d8f261237698cd2fd74019e357
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2022-01-15 16:44:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-https://teachablemachine.withgoogle.com/models/YYqTBHDTX/
+
+
+Webcam.set({
+    width:300,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+    camera = document.getElementById("cam");
+    Webcam.attach(camera);
+
+    function capture() {
+        Webcam.snap(function(data_uri){
+            document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+    }
+    console.log("ml5 version is",ml5.version);
+
+    var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/YYqTBHDTX/",modelloaded);
+    function modelloaded() {
+        console.log("model is loaded");
+    }
